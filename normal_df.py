7bbe8c70c1c220b11db8a9f699b63a93cd5f2bc2
Wamique Ansari <wamiqueansari@Wamiques-MacBook-Pro.local>
2019-02-10 18:58:44 -0800

    corrected the calcuation part and uploading the updated output

--- normal_df.py
+++ normal_df.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import math
 
 def normal_pdf(x, mu=0, sigma=1):
-    normal_dist = 1/(sigma*math.sqrt(2*math.pi))*math.exp(-(x-mu)**2/(2*sigma**2))
+    normal_dist = 1/(math.sqrt(2*math.pi*(sigma**2)))*math.exp(-(x-mu)**2/(2*sigma**2)) 
     return normal_dist
 
 xs = [x / 10.0 for x in range(-50, 50)]
