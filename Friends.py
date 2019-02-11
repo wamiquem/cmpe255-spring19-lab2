bac51b58370f484695dd6086a3697119a86f320f
Wamique Ansari <wamiqueansari@Wamiques-MacBook-Pro.local>
2019-02-10 16:33:27 -0800

    Adding comments and output's screenshots

--- Friends.py
+++ Friends.py
@@ -1,14 +1,19 @@
+import numpy
+
 num_friends=[100, 49, 41, 40, 25, 10, 5, 4, 7, 20, 60]
 
 def mean_num_friends(x):
     count_friends = len(x)
     sum = 0
     
-    for eachx in x:
-        sum = sum + eachx
+    for each_x in x:
+        sum = sum + each_x
     
     mean = sum/count_friends
-    print("mean = %.2f" %mean)
+    print("mean = %f" %mean)
+    
+    mean_numpy = numpy.mean(x)
+    print("mean using python's numpy lib= %f" %mean_numpy)
 
 def median_num_friends(x):
     x.sort()
@@ -18,11 +23,15 @@ def median_num_friends(x):
     if mod == 0:
         index = int(count_friends/2)
         median = (x[index-1]+x[index])/2
-        print("median = %.2f" %median)
     else:
         index = int(count_friends/2)
         median = x[index]
-        print("median = %d" %median)
+    
+    print("median = %f" %median)
+    
+    median_numpy = numpy.median(x)
+    print("median using python's numpy lib = %f" %median_numpy)
             
 mean_num_friends(num_friends)
+print("\n")
 median_num_friends(num_friends)
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # cmpe255-spring19-lab2
 
-For mean and median program, I have calculated mean and median  by implenting the actual logic as well as I have used python numpy library
+For mean and median program, I have calculated mean and median  by implenting the actual logic. Also, I have used python's numpy library
